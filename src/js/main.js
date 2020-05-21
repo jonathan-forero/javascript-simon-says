8bf290fcbe2e8e14b2dbea4f5781404ee592a5f8
Clonathan86 <forerojonathan@gmail.com>
2020-05-20 22:44:22 -0500

    Add verification of choosen color and add constants to avoid magic numbers

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,24 @@ const purple = document.getElementById('purple');
 const orange = document.getElementById('orange');
 const green = document.getElementById('green');
 const btnStart = document.getElementById('btnStart');
+const LAST_LEVEL = 10;
+const FIRST_LEVEL = 1;
+const FIRST_SUB_LEVEL = 0;
+const BUTTONS_AMOUNT = 4;
+const LIGHT_TIME = 350;
+const LIGHT_IDLE_TIME = 1000;
+const TIME_BETWEEN_LEVELS = 1500;
 
 class Game {
   constructor() {
     this.init();
     this.createSecuence();
-    this.setNextLevel();
+    setTimeout(this.setNextLevel, LIGHT_TIME);
   }
 
   init = () => {
     btnStart.classList.add('hide');
-    this.level = 1;
+    this.level = FIRST_LEVEL;
     this.colors = {
       blue,
       purple,
@@ -23,10 +30,11 @@ class Game {
   };
 
   createSecuence = () => {
-    this.secuence = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    this.secuence = new Array(LAST_LEVEL).fill(0).map(n => Math.floor(Math.random() * BUTTONS_AMOUNT));
   };
 
   setNextLevel = () => {
+    this.subLevel = FIRST_SUB_LEVEL;
     this.lightSequence();
     this.addClickEvents();
   };
@@ -44,19 +52,32 @@ class Game {
     }
   };
 
+  getNumberFromColor = color => {
+    switch (color) {
+      case 'blue':
+        return 0;
+      case 'purple':
+        return 1;
+      case 'orange':
+        return 2;
+      case 'green':
+        return 3;
+    }
+  };
+
   turnOffColor = color => {
     this.colors[color].classList.remove('light');
   };
 
   lightColor = color => {
     this.colors[color].classList.add('light');
-    setTimeout(() => this.turnOffColor(color), 350);
+    setTimeout(() => this.turnOffColor(color), LIGHT_TIME);
   };
 
   lightSequence = () => {
     for (let i = 0; i < this.level; i++) {
       const color = this.getColorFromNumber(this.secuence[i]);
-      setTimeout(() => this.lightColor(color), 1000 * i);
+      setTimeout(() => this.lightColor(color), LIGHT_IDLE_TIME * i);
     }
   };
 
@@ -67,9 +88,31 @@ class Game {
     this.colors.green.addEventListener('click', this.pickColor);
   };
 
+  removeClickEvents = () => {
+    this.colors.blue.removeEventListener('click', this.pickColor);
+    this.colors.purple.removeEventListener('click', this.pickColor);
+    this.colors.orange.removeEventListener('click', this.pickColor);
+    this.colors.green.removeEventListener('click', this.pickColor);
+  };
+
   pickColor = ev => {
-    console.log(this);
-    console.log(ev.target.id);
+    const pickedColorName = ev.target.dataset.color;
+    const pickedColorNumber = this.getNumberFromColor(pickedColorName);
+    this.lightColor(pickedColorName);
+    if (pickedColorNumber === this.secuence[this.subLevel]) {
+      this.subLevel++;
+      if (this.subLevel === this.level) {
+        this.level++;
+        this.removeClickEvents();
+        if (this.level === LAST_LEVEL + 1) {
+          // WON!
+        } else {
+          setTimeout(this.setNextLevel, TIME_BETWEEN_LEVELS);
+        }
+      }
+    } else {
+      // GAME OVER
+    }
   };
 }
 
